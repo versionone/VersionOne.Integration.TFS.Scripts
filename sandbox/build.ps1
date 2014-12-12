3511270b873890941cc34f79ecb784e59740002e
Laureano Remedi <lremedi@gmail.com>
2014-12-12 13:59:05 -0300

    Using new image with SQL full text search. Adding conditionals to whether install or not tfs integration. Changing to visual studio 2013 community.

--- sandbox/build.ps1
+++ sandbox/build.ps1
@@ -3,11 +3,9 @@ Param(
     [string]$vm_password,
     [string]$vm_name,
     [string]$azure_service_name,
-    [string]$new
+    [string]$new,
+    [string]$install_tfs_integration
 )
-
-(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
-Install-Module -ModuleUrl https://raw.githubusercontent.com/openagile/AzureInstanceTools/master/AzureInstanceTools.psm1 -update
 
 Write-Host "Starting execution at:"(Get-Date -Format g)
 
@@ -15,7 +13,8 @@ $secpasswd = ConvertTo-SecureString $vm_password -AsPlainText -Force
 $cred=New-Object System.Management.Automation.PSCredential ($vm_username, $secpasswd)
 
 if ($new -eq "true"){
-    $image_name = "sql2012exp-20140925-13769"
+    #$image_name = "sql2012exp-20140925-13769"
+    $image_name = "sqlftstemplate-202605-71354"
     Write-Host 'Removing previous VM'
     Remove-AzureVM -ServiceName $azure_service_name -Name $vm_name -DeleteVHD
     Remove-AzureService -ServiceName $azure_service_name -Force
@@ -38,7 +37,7 @@ $script_path_step4 = 'Configure-TfsListener.ps1'
 $boxstarterVM = Enable-BoxstarterVM -Provider azure -CloudServiceName $azure_service_name -VMName $vm_name -Credential $cred
 $boxstarterVM | Install-BoxstarterPackage -Package tfsexpress.standard -Credential $cred
 $boxstarterVM | Install-BoxstarterPackage -Package tfsexpress.build -Credential $cred
-$boxstarterVM | Install-BoxstarterPackage -Package VisualStudio2013Professional -Credential $cred
+$boxstarterVM | Install-BoxstarterPackage -Package visualstudiocommunity2013  -Credential $cred
 $boxstarterVM | Install-BoxstarterPackage -Package git -Credential $cred
 $boxstarterVM | Install-BoxstarterPackage -Package tfs2013powertools -Credential $cred
 
@@ -49,7 +48,7 @@ Write-Host "Setting sample data..."
 #$script_path_step1 = 'New-TeamProject.ps1'
 $CollectionUri = "http://localhost:8080/tfs/DefaultCollection"
 $ProjectName = "AnotherTeamProject"
-$ProcessTemplateName = "MSF for Agile Software Development 2013"
+$ProcessTemplateName = "MSF for Agile Software Development 2013.4"
 Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step1" `
 @($CollectionUri,$ProjectName,$ProcessTemplateName)
 
@@ -64,17 +63,19 @@ $tfs_automation_remote = "https://portalvhdsw36vjbsgqb26p.blob.core.windows.net/
 Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step2" `
 @($tfs_team_project_collection,$tfs_team_project, $tfs_build_name, $tfs_build_description, $tfs_workspace, $tfs_git_repository, $tfs_automation_remote)
 
-#$script_path_step3 = 'Install-TfsListener.ps1'
-$tfs_listener_remote = "https://v1integrations.blob.core.windows.net/downloads/VersionOne.Integration.Tfs.Listener.Installer.msi"
-Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step3" `
-@($tfs_listener_remote)
+if ($install_tfs_integration -eq "true"){
+    #$script_path_step3 = 'Install-TfsListener.ps1'
+    $tfs_listener_remote = "https://v1integrations.blob.core.windows.net/downloads/VersionOne.Integration.Tfs.Listener.Installer.msi"
+    Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step3" `
+    @($tfs_listener_remote)
 
-#$script_path_step4 = 'Configure-TfsListener.ps1'
-$Url="https://www14.v1host.com/v1sdktesting/"
-$UserName="remote"
-$Password="remote"
-$TfsUrl="http://$azure_service_name.cloudapp.net:8080/tfs/DefaultCollection/"
-Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step4" `
-@($azure_service_name,$Url,$UserName,$Password,$TfsUrl,$vm_username,$vm_password)
+    #$script_path_step4 = 'Configure-TfsListener.ps1'
+    $Url="https://www14.v1host.com/v1sdktesting/"
+    $UserName="remote"
+    $Password="remote"
+    $TfsUrl="http://$azure_service_name.cloudapp.net:8080/tfs/DefaultCollection/"
+    Invoke-RmtAzure "$vm_username" "$vm_password" "$vm_name" "$azure_service_name" "$script_path_step4" `
+    @($azure_service_name,$Url,$UserName,$Password,$TfsUrl,$vm_username,$vm_password)
+}
 
 Write-Host "Ending execution at:"(Get-Date -Format g)
